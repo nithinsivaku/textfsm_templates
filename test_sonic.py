ee77d9940362cce12cb6c2bbd473257caba678fb
nsivakumar <nsivakumar@juniper.net>
2022-12-08 08:24:00 -0500

    Modify template files

--- test_sonic.py
+++ test_sonic.py
@@ -1,4 +1,5 @@
 import textfsm
+from textfsm import clitable
 
 def test_show_chassis_hardware():
     hardware_file = open("hardware_info", "r")
@@ -12,9 +13,14 @@ def test_show_chassis_hardware():
     fsm_results = re_table.ParseText(hardware_info)
     
     print(re_table.header)
-    
+    for result in fsm_results:
+        print(result)
+    # cli_obj = clitable.CliTable("index", f)
+    # cli_obj.ParseCmd(hardware_info, raw_text=False)
     collection_of_results = [dict(zip(re_table.header, pr)) for pr in fsm_results]
     print(collection_of_results)
+
+#Routing_engine
 
 
 test_show_chassis_hardware()
